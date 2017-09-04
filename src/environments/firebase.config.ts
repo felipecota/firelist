881ad49f47fb3ee193daea2877125cfb29d30b10
felipecota <felipe_cota@hotmail.com>
2017-09-04 17:17:08 -0300

    First edit

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+  export const FirebaseConfig = {
+    apiKey: "AIzaSyAT0RaduvnW2j2B_wXq8wW7VN6I6Ju37hE",
+    authDomain: "simplesapprealtime-60890.firebaseapp.com",
+    databaseURL: "https://simplesapprealtime-60890.firebaseio.com",
+    projectId: "simplesapprealtime-60890",
+    storageBucket: "",
+    messagingSenderId: "598037447832"
+  };
