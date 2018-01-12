3f566eb963726c8773ace2dba15a9b7b5fd11bdd
felipecota <felipe_cota@hotmail.com>
2018-01-12 16:23:54 -0200

    Final version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
 export class AppComponent {  
 
   title = 'realtimeapp';
-  version = 'v6.5';
+  version = 'v6.6';
   isConnected: Observable<boolean>;
  
   constructor(
